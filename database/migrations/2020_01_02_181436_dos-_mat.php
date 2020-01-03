9fbdfd32753fdadd5a01e899be11a0caf4e3c7b4
Adnanmuhamad <1167050095@student.uinsgd.ac.id>
2020-01-03 21:09:51 +0700

    first commit

---
+++ database/migrations/2020_01_02_181436_dos-_mat.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DosMat extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Dos_mat', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('Id_Dosen');
+            $table->integer('Id_Matkul');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Dos_mat');
+    }
+}
